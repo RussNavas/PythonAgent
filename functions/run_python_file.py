013f1e0844e65bc4223787958420ec3e88a46b09
RussNavas <navasr@oregonstate.edu>
2025-10-09 13:59:26 -0700

    can run python scripts

---
+++ functions/run_python_file.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working = os.path.abspath(working_directory)
+    abs_given = os.path.abspath(os.path.join(working_directory, file_path))
+
+    work_prefix = os.path.join(abs_working, "")
+
+    if not (abs_given.startswith(work_prefix)):
+        return (f'Error: Cannot execute "{file_path}" as it is outside'
+                f'the permitted working directory.')
+
+    if not os.path.exists(abs_given):
+        return f'Error: File "{file_path}" not found.'
+
+    if file_path[-3:] != ".py":
+        return f'Error: "{file_path}" is not a Python file.'
+
+    cmd = [sys.executable, abs_given, *map(str, args)]
+    try:
+        result = subprocess.run(cmd,
+                                cwd=abs_working,
+                                timeout=30,
+                                capture_output=True,
+                                text=True)
+        result_stdout = result.stdout
+        result_stderr = result.stderr
+        res_string = (f'STDOUT: {result_stdout}\n'
+                      f'STDERR: {result_stderr}\n')
+
+        if result.returncode != 0:
+            res_string += f"Process exited with code {result.returncode}"
+        if not res_string:
+            return "No output produced"
+        return res_string
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
-
+from functions.run_python_file import run_python_file
 # dir content test
 """
 a = get_files_info("calculator", ".")
@@ -27,12 +27,23 @@ d = get_file_content("calculator", "pkg/does_not_exist.py") # (this should retur
 
 
 # Test to write to file
+"""
 a = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
 b = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
 c = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+"""
 
-
+# (should print the calculator's usage instructions)
+a = run_python_file("calculator", "main.py")
+# (should run the calculator... which gives a kinda nasty rendered result)
+b = run_python_file("calculator", "main.py", ["3 + 5"])
+c = run_python_file("calculator", "tests.py")
+# (this should return an error)
+d = run_python_file("calculator", "../main.py")
+# (this should return an error)
+e = run_python_file("calculator", "nonexistent.py")
 print(a)
 print(b)
 print(c)
-#  print(d)
+print(d)
+print(e)
