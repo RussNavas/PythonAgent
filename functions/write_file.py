e26ef28bf1a3767b05519618f2fb49ef1c10c3bd
RussNavas <navasr@oregonstate.edu>
2025-10-09 12:44:54 -0700

    can write files

---
+++ functions/write_file.py
@@ -0,0 +1,36 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    # collapse any use of .. or .
+    abs_working = os.path.abspath(working_directory)
+    # combine directory and file path while collapsing .. or .
+    abs_given = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # add os correct seperator to end of URI
+    work_prefix = os.path.join(abs_working, "")
+
+    # a descendant of allowed directory
+    if not (abs_given.startswith(work_prefix)):
+        return (f'Error: Cannot write "{file_path}" '
+                f'as it is outside the permitted working directory.')
+
+    # ensure a file was passed and not a dir
+    if os.path.isdir(abs_given):
+        return f'Error: "{file_path}" is a directory, not a file path'
+
+    # ensure parent directory exists
+    parent_dir = os.path.dirname(abs_given)
+    if parent_dir and not os.path.exists(parent_dir):
+        try:
+            os.makedirs(parent_dir, exist_ok=True)
+        except Exception:
+            return f'Error: makedirs for "{file_path}" failed.'
+    # try to write to file or create file
+    try:
+        with open(abs_given, "w", encoding="utf-8") as f:
+            f.write(content)
+        return (f'Successfully wrote to "{file_path}" ({len(content)} '
+                f'characters written)')
+    except Exception:
+        return f'Error: write to "{file_path}" failed.'
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
-
+from functions.write_file import write_file
 
 # dir content test
 """
@@ -18,12 +18,21 @@ print(d)
 # file content test
 # print(get_file_content("calculator", "lorem.txt"))
 
+"""
 a = get_file_content("calculator", "main.py"),
 b = get_file_content("calculator", "pkg/calculator.py"),
 c = get_file_content("calculator", "/bin/cat"),  # (this should return an error string)
 d = get_file_content("calculator", "pkg/does_not_exist.py") # (this should return an error string)
+"""
+
+
+# Test to write to file
+a = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+b = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+c = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+
 
 print(a)
 print(b)
 print(c)
-print(d)
+#  print(d)
